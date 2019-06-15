5d2df1242eaec9a8f1525b57df2b33fb6a93d463
Milad Mikeal <miladmikeal@gmail.com>
2019-06-14 21:08:35 -0700

    Set up login page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,8 +13,17 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer v-model="drawer" app class="indigo">
-      <p>test</p>
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -23,7 +32,11 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "person", text: "Profile", route: "/tasks" },
+        { icon: "dashboard", text: "Dashboard", route: "/" }
+      ]
     };
   }
 };
---
+++ src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container class="mt-5">
+    <v-layout row justify-center>
+      <v-flex xs12 md6>
+        <v-toolbar dark color="primary">
+          <v-icon>exit_to_app</v-icon>
+          <v-toolbar-title>Login</v-toolbar-title>
+        </v-toolbar>
+        <v-card class="pa-5">
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+            <v-text-field
+              :type="'password'"
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+            ></v-text-field>
+
+            <v-layout row>
+              <v-btn :disabled="!valid" color="success" @click="validate">Login</v-btn>
+              <v-spacer></v-spacer>
+              <span>
+                <a href="/signup">Need an account?</a>
+              </span>
+            </v-layout>
+          </v-form>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    email: "",
+    password: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ],
+    passwordRules: [v => !!v || "Password is required"],
+    select: null
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+        let email = this.email;
+        let password = this.password;
+        axios
+          .post("http://localhost:3000/users/login", {
+            email,
+            password
+          })
+          .then(() => this.$router.push("/dashboard"))
+          .catch(err => console.log(err));
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
